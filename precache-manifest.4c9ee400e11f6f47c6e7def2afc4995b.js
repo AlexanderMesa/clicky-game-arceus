e082b0ef00f2a5c9bb995bba9984552631f9dbda
ajmesa <amesa987@gmail.com>
2019-03-19 20:11:50 -0400

    Updates

---
+++ precache-manifest.4c9ee400e11f6f47c6e7def2afc4995b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49cb77536564b064f71a",
+    "url": "/clicky-game-arceus/static/css/main.b39ae314.chunk.css"
+  },
+  {
+    "revision": "49cb77536564b064f71a",
+    "url": "/clicky-game-arceus/static/js/main.f288aaa8.chunk.js"
+  },
+  {
+    "revision": "298c2c30bf6715acd3be",
+    "url": "/clicky-game-arceus/static/js/runtime~main.af374dc2.js"
+  },
+  {
+    "revision": "8704164c33768b7e4222",
+    "url": "/clicky-game-arceus/static/js/2.4230e7d3.chunk.js"
+  },
+  {
+    "revision": "f28ef43401078afe844b61824f6ebabc",
+    "url": "/clicky-game-arceus/index.html"
+  }
+];
